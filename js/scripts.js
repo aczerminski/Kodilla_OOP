f9857a95f16267f646ffef84e0747c25a4e2d856
Artur Czerminski <czermi@gmail.com>
2017-07-20 13:44:47 +0200

    kanban modified

--- js/scripts.js
+++ js/scripts.js
@@ -34,16 +34,16 @@ $(function() {
         .append($columnCardList);
 			return $column;
     }
-
-		Column.prototype = {
-    	addCard: function(card) {
-      this.$element.children('ul').append(card.$element);
-    	},
-    	removeColumn: function() {
-      this.$element.remove();
-    	}
-		};
   }
+	Column.prototype = {
+		addCard: function(card) {
+			this.$element.children('ul').append(card.$element);
+		},
+		removeColumn: function() {
+			this.$element.remove();
+		}
+	};
+
 	function Card(description) {
 		var self = this;
 
@@ -64,10 +64,10 @@ $(function() {
 						.append($cardDescription);
 			return $card;
 		}
-		Card.prototype = {
-			removeCard: function() {
-				this.$element.remove();
-			}
+	}
+	Card.prototype = {
+		removeCard: function() {
+			this.$element.remove();
 		}
 	}
 	var board = {
@@ -106,4 +106,4 @@ $(function() {
 
 	todoColumn.addCard(card1);
 	doingColumn.addCard(card2);
-})
+});
